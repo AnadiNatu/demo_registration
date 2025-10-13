0a1f1ea3c0191cee205de68bcdf875be0d8a91e8
Anadi <anadiaarti2001@gmail.com>
2025-10-13 19:28:17 +0530

    application files changed and configured

--- frontend/demo_registration/src/app/app.routes.ts
+++ frontend/demo_registration/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent : () => import('').then(m => m.)},
+    { path: 'signup', loadComponent : ()  => import('').then(m => m.)},
+
+    {path : '/' , loadComponent : () => import('').then(m => m.)},
+    {path : '**' , redirectTo: ''}
+];
