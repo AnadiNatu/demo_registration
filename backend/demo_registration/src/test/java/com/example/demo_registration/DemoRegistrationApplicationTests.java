b9134fe5a7e0ff36d4a13e37f92f658937f01287
Anadi <anadiaarti2001@gmail.com>
2025-10-13 17:53:34 +0530

    Intial Commit After Project Creation

---
+++ backend/demo_registration/src/test/java/com/example/demo_registration/DemoRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
